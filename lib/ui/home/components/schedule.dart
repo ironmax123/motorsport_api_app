0d4a9be968904776ae2726bd651442134baefa67
ironmax123 <appdeverop@gmail.com>
2026-02-23 21:52:54 +0900

    fix: オーバーフローを修正

--- lib/ui/home/components/schedule.dart
+++ lib/ui/home/components/schedule.dart
@@ -16,6 +16,8 @@ class HomeScreenSchedule extends StatelessWidget {
             _ScheduleCard(
               round: entry.round,
               circuit: entry.courseJp,
+              circuitEn: entry.courseEn,
+              location: entry.location,
               date: entry.date,
             ),
             const Gap(12),
@@ -29,22 +31,41 @@ class HomeScreenSchedule extends StatelessWidget {
 class _ScheduleCard extends StatelessWidget {
   final String round;
   final String circuit;
+  final String circuitEn;
+  final String location;
   final String date;
   const _ScheduleCard({
     required this.round,
     required this.circuit,
+    required this.circuitEn,
+    required this.location,
     required this.date,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 120,
-      height: 60,
+      width: 200,
+      height: 125,
       decoration: BoxDecoration(color: Colors.white),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Text(round), Text(circuit), Text(date)],
+        children: [
+          Text(
+            'Rd.$round',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            circuit,
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+          Text(circuitEn, style: TextStyle(fontSize: 12)),
+          Text('Location at: $location'),
+          Text(
+            date,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
